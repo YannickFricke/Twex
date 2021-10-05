a897aec1c0d2119af4fc28f75e171bc4ea8a1cbe
Yannick Fricke <yannickfricke@googlemail.com>
2021-10-05 12:59:05 +0200

    Added ex_doc as dependency + added the configuration for it

--- mix.exs
+++ mix.exs
@@ -7,7 +7,17 @@ defmodule Twex.MixProject do
       version: "0.0.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "Twex",
+      source_url: "https://github.com/YannickFricke/Twex",
+      homepage_url: "https://github.com/YannickFricke/Twex",
+      docs: [
+        # The main page in the docs
+        main: "Twex",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -22,8 +32,7 @@ defmodule Twex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
 end
