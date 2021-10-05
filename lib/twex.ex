6a56d99b99c9ae8ec447d584576074d42fe37cab
Yannick Fricke <yannickfricke@googlemail.com>
2021-10-05 11:40:05 +0200

    Remove default tests

--- lib/twex.ex
+++ lib/twex.ex
@@ -2,17 +2,4 @@ defmodule Twex do
   @moduledoc """
   Documentation for `Twex`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Twex.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/twex_test.exs
+++ test/twex_test.exs
@@ -1,8 +1,3 @@
 defmodule TwexTest do
   use ExUnit.Case
-  doctest Twex
-
-  test "greets the world" do
-    assert Twex.hello() == :world
-  end
 end
